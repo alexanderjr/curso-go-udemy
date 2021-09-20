d9a75190ff1fffeedd4d4eb8ee631c4b32790b01
Alex <aafonso.93@gmail.com>
2021-09-20 19:57:08 -0300

    Aula 24 - Panic/Recover

--- main.go
+++ main.go
@@ -2,13 +2,26 @@ package main
 
 import "fmt"
 
-func studentIsApproved(avg int) bool {
-	//a execucacao dessa funcao ira para o final da funcao, portanto executada antes do return
+func recoverWhenFailed() {
+	if fn := recover(); fn != nil {
+		fmt.Println("Recuperando fn que caiu no panic")
+	}
+}
+
+func studentIsApproved(n1, n2 int) bool {
+	defer recoverWhenFailed()
+	sum := n1 + n2
+
+	if sum == 0 {
+		panic("A soma das medias não pode ser <= 0!!!")
+	}
+	avg := n1 + n2/2
 	defer fmt.Println("O resultado sera mostrado a seguir")
 
 	return avg >= 5
 }
 
 func main() {
-	fmt.Print(studentIsApproved(5))
+	fmt.Println(studentIsApproved(0, 0))
+	fmt.Print("Fim")
 }
