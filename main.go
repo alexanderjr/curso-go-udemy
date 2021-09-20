31d61835f120854864265bb04acd06c3321d7c83
Alex <aafonso.93@gmail.com>
2021-09-20 20:42:58 -0300

    Aula 30 - Interfaces genericas

--- main.go
+++ main.go
@@ -2,32 +2,12 @@ package main
 
 import "fmt"
 
-type Shape interface {
-	area() float64
-}
-
-type Square struct {
-	side float64
-}
-
-func (s Square) area() float64 {
-	return s.side * s.side
-}
-
-type Triangle struct {
-	base, height float64
-}
-
-func (s Triangle) area() float64 {
-	return (s.base * s.height) / 2
-}
-
-func printArea(s Shape) {
-	fmt.Print(s.area())
-	fmt.Print("\n")
+func generic(a interface{}) {
+	fmt.Print(a)
 }
 
 func main() {
-	printArea(Square{5})
-	printArea(Triangle{5, 100})
+	generic(1)
+	generic("Alex")
+	generic(true)
 }
