a911b725c6f5d306321865c4f69519bea8ad5133
Alex <aafonso.93@gmail.com>
2021-09-20 20:02:56 -0300

    Aula 25 - Closure

--- main.go
+++ main.go
@@ -2,26 +2,20 @@ package main
 
 import "fmt"
 
-func recoverWhenFailed() {
-	if fn := recover(); fn != nil {
-		fmt.Println("Recuperando fn que caiu no panic")
+func closure() func() {
+	text := "Clojure function"
+
+	fn := func() {
+		fmt.Print(text)
 	}
-}
 
-func studentIsApproved(n1, n2 int) bool {
-	defer recoverWhenFailed()
-	sum := n1 + n2
-
-	if sum == 0 {
-		panic("A soma das medias não pode ser <= 0!!!")
-	}
-	avg := n1 + n2/2
-	defer fmt.Println("O resultado sera mostrado a seguir")
-
-	return avg >= 5
+	return fn
 }
 
 func main() {
-	fmt.Println(studentIsApproved(0, 0))
-	fmt.Print("Fim")
+	text := "Main function "
+	fmt.Print(text)
+
+	fn := closure()
+	fn()
 }
