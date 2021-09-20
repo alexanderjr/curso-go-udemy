6ea3edc367220ad1d6022582f7b4ce74c14b1f81
Alex <aafonso.93@gmail.com>
2021-09-20 20:38:37 -0300

    Aula 29 - Interface

--- main.go
+++ main.go
@@ -2,21 +2,32 @@ package main
 
 import "fmt"
 
-type user struct {
-	name string
-	age  int
+type Shape interface {
+	area() float64
 }
 
-func (u user) isAdult() bool {
-	return u.age >= 18
+type Square struct {
+	side float64
 }
 
-func (u *user) incrementAge() {
-	u.age++
+func (s Square) area() float64 {
+	return s.side * s.side
+}
+
+type Triangle struct {
+	base, height float64
+}
+
+func (s Triangle) area() float64 {
+	return (s.base * s.height) / 2
+}
+
+func printArea(s Shape) {
+	fmt.Print(s.area())
+	fmt.Print("\n")
 }
 
 func main() {
-	u := user{"Alexander", 17}
-	u.incrementAge()
-	fmt.Print(u, u.isAdult())
+	printArea(Square{5})
+	printArea(Triangle{5, 100})
 }
