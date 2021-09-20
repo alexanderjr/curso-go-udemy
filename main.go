7e778c8db00ccec52f38cb8be286d542b3f8be03
Alex <aafonso.93@gmail.com>
2021-09-20 19:42:10 -0300

    Aula 20

--- main.go
+++ main.go
@@ -2,18 +2,11 @@ package main
 
 import "fmt"
 
-func fibonnaci(posicao uint) uint {
-	if posicao <= 1 {
-		return posicao
+func write(text string, numbers ...int) {
+	for c := 0; c < len(numbers); c++ {
+		fmt.Printf("%s %d \n", text, numbers[c])
 	}
-
-	return fibonnaci(posicao-2) + fibonnaci(posicao-1)
 }
-
 func main() {
-
-	posicao := uint(7)
-	for i := uint(0); i <= posicao; i++ {
-		fmt.Printf("%d ", fibonnaci(i))
-	}
+	write("Hello World", 1, 2, 3)
 }
